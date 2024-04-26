bbe5e9efcb85bd32f6c7bfa45d27850360702617
malithMk2000 <164366746+malithMk2000@users.noreply.github.com>
2024-04-26 09:19:09 +0000

    add a button

---
+++ malith-frontend/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>
+      Scheduling Poll
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Book Lab Session</h1>
+      <MyButton />
+    </div>
+  );
+}
