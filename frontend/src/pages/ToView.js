769bbfcd7c022c2621c8ab07ffcad578910677da
SalindaHulangamuwa <150468057+SalindaHulangamuwa@users.noreply.github.com>
2024-07-22 00:32:14 +0530

    correction in ToView

--- frontend/src/pages/ToView.js
+++ frontend/src/pages/ToView.js
@@ -13,57 +13,71 @@ export default function CalendarViewTo() {
   const [events, setEvents] = useState([]);
   const [selectedEvent, setSelectedEvent] = useState(null);
   const [isBoxVisible, setIsBoxVisible] = useState(false);
-  const [selectedMonth, setSelectedMonth] = useState(moment().format('YYYY-MM'));
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [attendeeTypes, setAttendeeTypes] = useState({});
   const profileRef = useRef(null);
+  const token = localStorage.getItem('token');
+  const [selectedMonth, setSelectedMonth] = useState(moment().format('YYYY-MM'));
 
   useEffect(() => {
     const fetchBookings = async () => {
       try {
-        const token = localStorage.getItem('token');
         const response = await axios.get('/api/bookings', {
           headers: {
             'Authorization': `Bearer ${token}`
           }
         });
-        const formattedEvents = response.data.map(booking => ({
-          id: booking._id,
+        setEvents(response.data.map(booking => ({
+          id: booking._id, // Add booking ID for deletion
+          title: booking.title,
           start: new Date(booking.startTime),
           end: new Date(booking.endTime),
-          title: booking.title,
-          description: booking.description
-        }));
-        setEvents(formattedEvents);
+          description: booking.description,
+          attendees: booking.attendees // Assuming attendees is an array of strings
+        })));
       } catch (error) {
         console.error('Error fetching bookings:', error);
       }
     };
 
     fetchBookings();
-  }, []);
+  }, [token]);
 
-  const handleSelectEvent = (event) => {
+  const handleSelectEvent = async (event) => {
     setSelectedEvent(event);
+    try {
+      const response = await axios.get(`/api/notification/attendeesAndTypeByBookingId/${event.id}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      });
+      const attendeeTypesData = response.data.reduce((acc, obj) => {
+        const [email, type] = Object.entries(obj)[0];
+        acc[email] = type;
+        return acc;
+      }, {});
+      setAttendeeTypes(attendeeTypesData);
+      console.log(attendeeTypesData)
+    } catch (error) {
+      console.error('Error fetching attendee types:', error);
+    }
   };
 
-  const handleMonthChange = (e) => {
-    setSelectedMonth(e.target.value);
-  };
-
-  const CustomToolbar = ({ label }) => {
+  const CustomToolbar = () => {
     return (
       <div className="rbc-toolbar" style={{ backgroundColor: '#A6BBC1', padding: '10px' }}>
         <div style={{ color: 'red', display: 'flex', justifyContent: 'flex-start', width: '100%' }}>
           <div style={{ marginLeft: '20px' }}>
             <select
               value={selectedMonth}
-              onChange={handleMonthChange}
+              onChange={(e) => setSelectedMonth(e.target.value)}
               style={{ padding: '5px', fontSize: '16px' }}
             >
               {Array.from({ length: 12 }).map((_, i) => {
                 const month = moment().month(i).format('YYYY-MM');
                 return (
                   <option key={month} value={month}>
-                    {moment(month, 'YYYY-MM').format('MMMM YYYY')}
+                    {moment(month).format('MMMM YYYY')}
                   </option>
                 );
               })}
@@ -74,67 +88,76 @@ export default function CalendarViewTo() {
     );
   };
 
-  const customEventStyleGetter = (event, start, end, isSelected) => {
-    const style = {
-      backgroundColor: '#00b528',
-      color: '#fff',
-      borderRadius: '0px',
-      border: 'none',
-      display: 'block',
-      position: 'relative',
-      textAlign: 'center',
-      height: '100%',
-      width: '100%'
-    };
+  const handleUserIconClick = () => {
+    setIsBoxVisible(!isBoxVisible);
+  };
 
-    if (!event.start || !event.end) {
-      return { style };
+  const handleClickOutside = (event) => {
+    if (profileRef.current && !profileRef.current.contains(event.target)) {
+      setIsBoxVisible(false);
     }
+  };
 
-    return {
-      style,
-      children: (
-        <div style={{ height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'center' }}></div>
-      ),
+  useEffect(() => {
+    if (isBoxVisible) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
     };
-  };
+  }, [isBoxVisible]);
 
-  const customFormats = {
-    dayFormat: (date, culture, localizer) => localizer.format(date, 'dddd', culture),
-    weekdayFormat: (date, culture, localizer) => localizer.format(date, 'dddd', culture)
-  };
+  // Effect to update currentDate when selectedMonth changes
+  useEffect(() => {
+    const newDate = moment(selectedMonth, 'YYYY-MM').toDate();
+    setCurrentDate(newDate);
+  }, [selectedMonth]);
 
   return (
     <div>
-      <ToHeader onUserIconClick={() => setIsBoxVisible(!isBoxVisible)} isProfileVisible={isBoxVisible} />
+      <ToHeader onUserIconClick={handleUserIconClick} isProfileVisible={isBoxVisible}/>
       <div className='view_body'>
-        <div className='calendar-container'>
-          <div className='calendar-wrapper'>
+        <div style={{ padding: '50px' }}>
+          <div style={{ backgroundColor: 'white' }}>
             <Calendar
               localizer={localizer}
               events={events}
               startAccessor="start"
               endAccessor="end"
-              style={{ height: '800px' }}
-              formats={customFormats}
-              eventPropGetter={customEventStyleGetter}
+              style={{ height: '600px' }}
+              date={currentDate}
+              onNavigate={(date) => setCurrentDate(date)}
+              eventPropGetter={(event, start, end, isSelected) => ({
+                style: {
+                  backgroundColor: '#00B528', // Green color
+                },
+              })}
               onSelectEvent={handleSelectEvent}
               components={{
                 toolbar: CustomToolbar,
               }}
-              date={moment(selectedMonth, 'YYYY-MM').toDate()}
-              dayPropGetter={date => ({
-                className: 'rbc-day-red',
-              })}
             />
           </div>
           {selectedEvent && (
             <div className="event-details">
-              <button className="close-button" onClick={() => setSelectedEvent(null)}>×</button>
+              <div className="view-close-button" onClick={() => setSelectedEvent(null)}>&times;</div> {/* Add close button */}
               <h3>{selectedEvent.title}</h3>
               <p>{selectedEvent.description}</p>
               <p>Start: {selectedEvent.start.toLocaleString()}</p>
               <p>End: {selectedEvent.end.toLocaleString()}</p>
+              <p>Attendees:</p>
+              <div className="attendees-list">
+                {selectedEvent.attendees.map((attendee, index) => (
+                  <div
+                    key={index}
+                    className={`attendee-box ${attendeeTypes[attendee]}`}
+                  >
+                    {attendee}
+                  </div>
+                ))}
+              </div>
             </div>
           )}
         </div>
